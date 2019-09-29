7225419a3079d725b6e4074b62c5e20fc7daf398
Roman Zhuk <hokeruzu@gmail.com>
2019-09-30 01:29:35 +0300

    upd

---
+++ tabs/forms.py
@@ -0,0 +1,64 @@
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Optional
+from tabs.models import User
+from tabs import bcrypt
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
+    confirm_password = PasswordField('Confirm password',
+                                     validators=[DataRequired(), Length(min=3, max=20), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        print('validate_username')
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already taken.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=3, max=20)])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    old_password = PasswordField('Old password', validators=[DataRequired(), Length(min=3, max=20)])
+    new_password = PasswordField('New password', validators=[Optional(), Length(min=3, max=20)])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username is already taken.')
+
+    def validate_old_password(self, old_password):
+        if not bcrypt.check_password_hash(current_user.password, old_password.data):
+            raise ValidationError('Old password is wrong!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email is already taken.')
+
+class Add_tab(FlaskForm):
+    tab_name = StringField('Name')
+    url = StringField('URL', validators=[DataRequired(), Length(min=4)])
+    comment = TextAreaField('Comment')
+    comment_as_name = BooleanField('Use comment as a tab name')
+    submit = SubmitField('Create')
